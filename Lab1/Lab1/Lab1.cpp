9ab48180a2f0dfba420a63da37b4ca8a2e061cde
Kaiyuan Yu <kaiyuany@usc.edu>
2022-05-24 04:43:30 -0700

    done with dfs

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <unordered_set>
 #include <unordered_map>
-#include <queue>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -18,12 +19,56 @@ double compute_edge_weight(Node* node1, Node* node2) {
     return abs(node1->score - node2->score) / (node1->score + node2->score);
 }
 
+void print_graph(unordered_map<string, Node*> graph) {
+    for (auto n : graph) {
+        string name = n.first;
+        Node* node = n.second;
+        cout << name << " = " << node->score << ": ";
+        for (Node* neighbour : node->neighbours) {
+            cout << neighbour->name << " - ";
+        }
+        cout << endl;
+    }
+}
+
+void dfs(Node* source, Node* dest, int max_hop, double distance, unordered_set<string> & visited, vector<double> & distance_list, vector<string> & cur_path, unordered_map<double, string> & path_list) {
+    //base case
+    if (max_hop < 0) {
+        return;
+    }
+    if (source->name == dest->name) {
+        distance_list.push_back(distance);
+        string path = "";
+        for (string node : cur_path) {
+            path.append(node + " ");
+        }
+        path.pop_back();
+        path_list[distance] = path;
+        //cout << "adding [" << distance << ", " << path << "]" << endl;
+        return;
+    }
+    //recursive steps
+    vector<Node*> neighbours = source->neighbours;
+    for (Node* neighbour : neighbours) {
+        if (visited.find(neighbour->name) == visited.end()) {
+            double gap = compute_edge_weight(source, neighbour);
+            distance += gap;
+            visited.insert(neighbour->name);
+            cur_path.push_back(neighbour->name);
+            dfs(neighbour, dest, max_hop - 1, distance, visited, distance_list, cur_path, path_list);
+            cur_path.pop_back();
+            visited.erase(neighbour->name);
+            distance -= gap;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     string source = argv[1];
     string dest = argv[2];
-    int max_hops = stoi(argv[3]);
-    cout << "source node = " << source << ", dest node = " << dest << ", hops = " << max_hops << endl;
+    int max_hop = stoi(argv[3]);
+    //cout << "source node = " << source << ", dest node = " << dest << ", hops = " << max_hop << endl;
 
     unordered_map<string, Node*> graph;
 
@@ -31,8 +76,8 @@ int main(int argc, char* argv[])
     ifstream graph_file("graph.txt");
     string line;
     if (graph_file.is_open()) {
-        cout << "success" << endl;
         while (getline(graph_file, line)) {
+            //cout << line << ";" << endl;
             stringstream ss(line);
             string node1, node2;
             ss >> node1;
@@ -57,6 +102,58 @@ int main(int argc, char* argv[])
         cout << "failed when opening graph.txt" << endl;
         return -1;
     }
+
+    //read score file
+    ifstream score_file("scores.txt");
+    if (score_file.is_open()) {
+        while (getline(score_file, line)) {
+            //cout << line << ";" << endl;
+            stringstream ss(line);
+            string node;
+            double score;
+            ss >> node;
+            ss >> score;
+            //cout << "node = " << node << ", score = " << score << endl;
+            graph[node]->score = score;
+        }
+        score_file.close();
+    }
+    else{
+        cout << "failed when opening scores.txt" << endl;
+        return -2;
+    }
+
+    //print_graph(graph);
+    //find shortest path
+    unordered_set<string> visited;
+    vector<double> distance_list;
+    vector<string> cur_path;
+    unordered_map<double, string> path_list;
+
+    visited.insert(source);
+    cur_path.push_back(source);
+
+    dfs(graph[source], graph[dest], max_hop, 0, visited, distance_list, cur_path, path_list);
+
+    for (auto path : path_list) {
+        cout << "[" << path.first << ", " << path.second << "]" << endl;
+    }
+    //post processing
+    if (distance_list.size() == 0) {
+        cout << "No path exists between " << source << " and " << dest << " within " << max_hop << " hops." << endl;
+        return 1;
+    }
+    
+    double min = INT_MAX;
+    for (double distance : distance_list) {
+        if (distance < min) {
+            min = distance;
+        }
+    }
+    string path = path_list[min];
+
+    cout << path << endl;
+    cout << fixed << setprecision(2) << min << endl;
 
     return 0;
 }
