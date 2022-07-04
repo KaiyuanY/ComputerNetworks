d4cf5a07b8b5b65a2c56cd8a5d11c55d1ac23c11
Kaiyuan Yu <kaiyuany@usc.edu>
2022-07-04 09:49:23 -0700

    connection between client and main server working

--- Project/client.cpp
+++ Project/client.cpp
@@ -1,17 +1,76 @@
 #include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE 1024
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2) {//check wallet
+    srand(time(0));
+    int port = 20000 + rand() %100;
+    int server_port = 25080;
 
+    int client;
+    char buffer[BUFFER_SIZE];
+    std::string server_ip = "127.0.0.1";
+
+    sockaddr_in server_addr;
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if(client < 0)
+    {
+        std::cout << "failed to create socket." << std::endl;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr);
+
+    if (connect(client,(sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        std::cout << "failed to connect to the server on port " << server_port << std::endl;
+    }
+
+    //the connection is succesful, lets send data.
+    std::cout << "The client is up and running." << std::endl;
+    if (argc == 2) {//check wallet
+        std::string name = argv[1];
+        std::cout << name + " sent a balance enquiry request to the main server." << std::endl;
+
+        std::string data = "B " + name; // B for balance
+        strcpy(buffer, data.c_str());
+        //std::cout << "buffer = [ " << buffer << " ]" << std::endl; 
+        send(client, buffer, BUFFER_SIZE, 0);
+        recv(client, buffer, BUFFER_SIZE, 0);
+        std::string data_recv (buffer);
+        std::cout << data_recv << std::endl;
+
+        close(client);
+        return 1;
     }
     else if (argc == 4) {//transfer
+        std::string from = argv[1];
+        std::string to = argv[2];
+        std::string amount = argv[3];
+        std::cout << from + " has requested to transfer " + amount + " txcoins to " + to + "." << std::endl;
 
+        std::string msg = "T " + from + " " + to + " " + amount;
+        strcpy(buffer, msg.c_str());
+        send(client, buffer, BUFFER_SIZE, 0);
+        recv(client, buffer, BUFFER_SIZE, 0);
+        std::string msg_recv (buffer);
+        
     }
     else {
         //unrecognized argc
         std::cout << "Wrong number of arguents, please check your inputs." << std::endl;
     }
+    return 0;
 }
 
 
--- Project/serverM.cpp
+++ Project/serverM.cpp
@@ -3,65 +3,134 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <cstdlib>
 #include <string>
 #include <iostream>
+#include <pthread.h>
+#include <sstream>
 
-int main()
+#define BUFFER_SIZE 1024
+//int client, monitor, serverA, serverB, serverC;
+int addr_size;
+
+void *handle_client_request(void *p_client_socket)
 {
-    //create socket
-    int listening = socket(AF_INET, SOCK_STREAM, 0);
-    if(listening < 0)
+    int client_socket = *((int*)p_client_socket);
+    delete (int*)p_client_socket;
+
+    sockaddr_in client_addr;
+    char buffer[BUFFER_SIZE];
+
+    while(true)
     {
-        std::cout << "serverM socket failed." << std::endl;
+        std::cout << "waiting for connection..." << std::endl;
+
+        int size = sizeof(sockaddr_in);
+        int server = accept(client_socket, (sockaddr *)&client_addr, (socklen_t*)&size);
+
+        recv(server, buffer, BUFFER_SIZE, 0);
+
+        std::cout << buffer << std::endl;
+        
+        std::string data (buffer);
+        std::stringstream ss(data);
+        std::string word;
+        
+        ss >> word;
+        if(word == "B")//check balance
+        {
+            std::string name;
+            ss >> name;
+            //pull the balance from back servers.
+            
+            std::cout << "check balance:" << name << std::endl;
+            std::string response = "The balance of " + name + " is XXX.";
+            strcpy(buffer, response.c_str());
+            send(server, buffer, BUFFER_SIZE, 0);
+            close(server);
+        }
+        else//transfer
+        {
+            std::string from, to, amount;
+            ss >> from;
+            ss >> to;
+            ss >> amount;
+            //do something with the transfer
+
+            std::string response = "transfered " + amount + " from " + from + " to " + to;
+            strcpy(buffer, response.c_str());
+            send(server, buffer, BUFFER_SIZE, 0);
+            close(server);
+        }
+
+    }
+}
+
+
+int setup_socket(int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        std::cout << "establishing socket failed." << std::endl;
         return -1;
     }
 
-    //bind the socket to a ip/port
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(25000 + 080);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+    sockaddr_in server_addr;
 
-    if(bind(listening, AF_INET, (socketaddr*)&hint, sizeof(hint)) == -1)
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    if ((bind(sock, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
     {
-        std::cout << "binding failed serverM." << std::endl;
+        std::cout << "bind error." << std::endl;
+        return -1;
+    }
+
+    if(listen(sock, 10) < 0)
+    {
+        std::cout << "listen error." << std::endl;
+        return -1;
+    }
+
+    return sock;
+}
+
+int main()
+{
+    int client, monitor, serverA, serverB, serverC;
+    int port_client_local = 25080;
+    int port_monitor_local = 26080;
+    int port_udp_out = 24080;
+    int port_serverA = 21080, port_serverB = 22080, port_serverC = 23080;
+
+    addr_size = sizeof(sockaddr_in);
+
+    client = setup_socket(port_client_local);
+    if(client < 0)
+    {
+        std::cout << "client socket failed." << std::endl;
+        return -1;
+    }
+    monitor = setup_socket(port_monitor_local);
+    if(monitor < 0)
+    {
+        std::cout << "monitor socket failed." << std::endl;
         return -2;
     }
+    std::cout << "The main server is up and running." << std::endl;
 
-    //mark the socket for listening in
-    if(listen(listening, SOMAXCONN) == -1)
+    //handle client connection in a different thread
+    pthread_t t_client;
+    int *p_client = new int;
+    *p_client = client;
+    pthread_create(&t_client, NULL, handle_client_request, p_client);
+
+    //handle monitor connection
+    while(true)
     {
-        std::cout << "listen failed serverM." << std::endl;
-        return -3;
-    }
-
-    //accept a call
-    sockaddr_in client;
-    socklen_t = clientSize;
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
-
-    int clientSocket = accept(listening, (socketaddr*)&client, &clientSize);
-
-    if(clientSocket == -1)
-    {
-        std::cout << "client connection failed serverM." << std::endl;
-        return -4;
-    }
-
-    close(listening);
-
-    memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
-
-    int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0);
-    if(result)
-    {
-        std::cout << host << " connected on " << service << std::endl;
-    }
-    else
-    {
-        std::cout << "" << std::endl;
+        sleep(5);
     }
 
     return 0;
